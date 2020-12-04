6b295e6ab87546ba61c578a482808e59239cfc20
John Jeffrey <johnwd@gsda.com>
2020-12-04 17:00:51 +0100

    First push

---
+++ crack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+passwd="\$6\$fsfOJy/N\$NeaIKnMtXzJpQnWGXsBDlPJTgVs5iMrNEXTaHIjv3Oq5E9cQFOiCn7EJbbmxHGsdDCN86AReZh2d43WboHlOg1"
+dict="SecLists/Passwords/darkweb2017-top1000.txt" #Dictionaire
+
+while IFS= read -r line
+do
+	hashed=`mkpasswd -m SHA-512 -S fsfOJy/N $line` #-m algorithm -S salt
+	if [ "$hashed" = "$passwd" ]; then
+		echo $line
+	fi
+done < "$dict"
